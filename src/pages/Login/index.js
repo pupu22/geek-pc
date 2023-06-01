a3cea5edacc9a1a62720a89a4ac855c9f7445b4b
pupu22 <1591491800@qq.com>
2023-06-01 20:01:54 +0800

    添加antd中表单的验证功能

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,14 +5,34 @@ function Login (){
         <div className="login">
             <Card className="login-container">
                 <div className='login-font'>账号登录</div>
-                <Form>
-                    <Form.Item>
+                <Form validateTrigger= {['onBlur', 'onChange']}>
+                    <Form.Item
+                        name="mobile"
+                        rules={[
+                            {
+                                pattern:/^1[3-9]\d{9}$/,
+                                message: '手机号码格式不对',
+                                validateTrigger:'onBlur'
+                            },{
+                                required: true, message: '请输入手机号'
+                            }
+                        ]}
+                    >
                         <Input size='large' placeholder='请输入手机号'></Input>
                     </Form.Item>
-                    <Form.Item>
-                        <Input size='large' placeholder='请输入验证码'/>
+                    <Form.Item
+                        name='code'
+                        rules={[
+                            {
+                                len: 6, message: '验证码6个字符', validateTrigger: 'onBlur'
+                            },{
+                                required: true, message: '请输入验证码'
+                            }
+                        ]}
+                    >
+                        <Input size='large' placeholder='请输入验证码' maxLength={6}/>
                     </Form.Item>
-                    <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked">
                         <Checkbox className='login-checkbox-label'>
                             我已阅读并同意「用户协议」和「隐私条款」
                         </Checkbox>
