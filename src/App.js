038b5ef65be2dc4f06661ec4afc156917db353c0
pupu22 <1591491800@qq.com>
2023-06-02 15:42:40 +0800

    侧边栏的路由跳转与高亮控制

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Layout from './pages/Layout';
 import {AuthRoute} from './components/AuthRoute';
-
+import './App.css'
+import Article from './pages/Article';
+import Home from './pages/Home';
+import Publish from './pages/Publish';
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
            {/* 需要鉴权的路由 */}
-          <Route path='/*' element= {<AuthRoute><Layout/></AuthRoute>}></Route>
+          <Route path='/' element= {<AuthRoute><Layout/></AuthRoute>}>
+            <Route index element= {<Home/>}></Route>
+            <Route path="article" element= {<Article/>}></Route>
+            <Route path="publish" element= {<Publish/>}></Route>
+          </Route>
           <Route path='/login' element={<Login/>}></Route>
         </Routes>
       </div>    
