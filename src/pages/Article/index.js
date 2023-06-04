9a5726ac46fd83d3b4d13434e98bc59ebf55090f
pupu22 <1591491800@qq.com>
2023-06-04 19:32:21 +0800

    通过接口获取文章频道数据

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,9 +1,92 @@
-import { Breadcrumb, Card, Form, Button, Radio,Select ,DatePicker  } from "antd"
+import { Breadcrumb, Card, Form, Button, 
+    Radio,Select ,DatePicker,Table, Tag, Space} from "antd"
 import {Link} from 'react-router-dom'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+import { useEffect, useState } from "react"
+import { http } from "@/utils"
+
 
 const Article = () =>  {
     const { Option } = Select
     const { RangePicker } = DatePicker
+    const columns = [
+        {
+          title: '封面',
+          dataIndex: 'cover',
+          width:120,
+          render: cover => {
+            return <img src={cover} width={80} height={60} alt="" />
+          }
+        },
+        {
+          title: '标题',
+          dataIndex: 'title',
+          width: 220
+        },
+        {
+          title: '状态',
+          dataIndex: 'status',
+          render: data => <Tag color="green">审核通过</Tag>
+        },
+        {
+          title: '发布时间',
+          dataIndex: 'pubdate'
+        },
+        {
+          title: '阅读数',
+          dataIndex: 'read_count'
+        },
+        {
+          title: '评论数',
+          dataIndex: 'comment_count'
+        },
+        {
+          title: '点赞数',
+          dataIndex: 'like_count'
+        },
+        {
+          title: '操作',
+          render: data => {
+            return (
+              <Space size="middle">
+                <Button type="primary" shape="circle" icon={<EditOutlined />} />
+                <Button
+                  type="primary"
+                  danger
+                  shape="circle"
+                  icon={<DeleteOutlined />}
+                />
+              </Space>
+            )
+          }
+        }
+      ]
+    
+    const data = [
+        {
+            id: '8218',
+            comment_count: 0,
+            cover: {
+                images:['http://geek.itheima.net/resources/images/15.jpg'],
+            },
+            like_count: 0,
+            pubdate: '2019-03-11 09:00:00',
+            read_count: 2,
+            status: 2,
+            title: 'wkwebview离线化加载h5资源解决方案' 
+        }
+    ]
+    
+    //获取频道列表
+    const [channels, setChannels] = useState([])
+    useEffect(()=>{
+        async function fetchChannels(){
+            const res = await http.get('/channels')
+            setChannels(res.data.channels)
+        }
+        fetchChannels()
+    }, [])
+
     return (
         <div>
             <Card
@@ -28,14 +111,13 @@ const Article = () =>  {
                     </Radio.Group>
                 </Form.Item>
 
-                <Form.Item label="频道" name="channel_id">
-                    <Select
-                    placeholder="请选择文章频道"
-                    defaultValue="lucy"
-                    style={{ width: 120 }}
-                    >
-                    <Option value="jack">Jack</Option>
-                    <Option value="lucy">Lucy</Option>
+                <Form.Item label="频道" name="channel_id" >
+                    <Select placeholder="请选择文章频道" style={{width:200}}>
+                    {channels.map(item => (
+                        <Option key={item.id} value={item.id}>
+                            {item.name}
+                        </Option>
+                    ))}
                     </Select>
                 </Form.Item>
 
@@ -52,6 +134,9 @@ const Article = () =>  {
                     </Button>
                 </Form.Item>
             </Form>
+            </Card>
+            <Card title={`根据筛选条件共查询到 count 条结果：`}>
+                <Table rowKey="id" columns={columns} dataSource={data} />
             </Card>
         </div>
     )
